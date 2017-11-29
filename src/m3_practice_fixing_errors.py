83e0ebab042c84e5242b54ba81a4ebd99e8dfbdc
donge1114 <donge@rose-hulman.edu>
2017-11-29 13:59:59 -0500

    DONE

--- src/m3_practice_fixing_errors.py
+++ src/m3_practice_fixing_errors.py
@@ -6,7 +6,7 @@ Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
 """  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 ########################################################################
-# TODO: 2.
+# DONE: 2.
 #   Locate the syntax (notation) errors in this file
 #   by looking for red underlines.
 #
@@ -30,7 +30,7 @@ Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
 ########################################################################
 
 import rosegraphics as rg
-import math
+from math import cos, sin, pi
 
 
 def main():
@@ -45,7 +45,7 @@ def print_math():
     print(x)
 
     y = sin(pi)
-    print(The sine of PI is, y)
+    print('The sine of PI is', y)
 
 
 def turtle_fun():
@@ -60,9 +60,9 @@ def turtle_fun():
     alan.pen = rg.Pen('blue', 30)
     alan.paint_bucket = rg.PaintBucket('yellow')
 
-    alan.back(3 * (47 + 16))
+    alan.backward(3 * (47 + 16))
     alan.begin_fill()
-    alan.circle(25)
+    alan.draw_circle(25)
     alan.end_fill()
 
     alan.forward(200)
